148e96943a020537c35015ab3fa2f29a1c54498a
Weilao <qqq123026689@126.com>
2016-05-31 23:39:15 +0800

    rename private functions

--- src/vue-swiper.vue
+++ src/vue-swiper.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="swiper">
-        <div class="swiper-wrap" v-el:swiper-wrap
+        <div class="swiper-wrap"
+             v-el:swiper-wrap
              :class="{ 'dragging': dragging }"
              :style="{transform: 'translateY(' + translateY + 'px)'}"
-             @touchstart="onTouchStart"
-             @mousedown="onTouchStart"
+             @touchstart="_onTouchStart"
+             @mousedown="_onTouchStart"
              @transitionend="_onTransitionEnd">
             <slot></slot>
         </div>
@@ -27,45 +28,11 @@
             };
         },
         ready() {
-            this.onTouchMove = this.onTouchMove.bind(this);
-            this.onTouchEnd = this.onTouchEnd.bind(this);
+            this._onTouchMove = this._onTouchMove.bind(this);
+            this._onTouchEnd = this._onTouchEnd.bind(this);
             this.childrenCount = this.$el.querySelectorAll('.swiper-wrap>div').length;
         },
         methods: {
-            onTouchStart(e) {
-                this.startY = this.getPageY(e);
-                this.height = this.$el.clientHeight;
-                this.startTranslateY = this.translateY;
-                this.startTime = new Date().getTime();
-                this.dragging = true;
-                document.addEventListener('touchmove', this.onTouchMove);
-                document.addEventListener('touchend', this.onTouchEnd);
-                document.addEventListener('mousemove', this.onTouchMove);
-                document.addEventListener('mouseup', this.onTouchEnd);
-            },
-            onTouchMove(e) {
-                this.deltaY = this.getPageY(e) - this.startY;
-
-                if (!this.performanceMode) {
-                    this.translateY = this.startTranslateY + this.deltaY;
-                }
-            },
-            onTouchEnd(e) {
-                this.dragging = false;
-                var isQuickAction = new Date().getTime() - this.startTime < 1000;
-                if (this.deltaY < -100 || (isQuickAction && this.deltaY < -15)) {
-                    this.next();
-                } else if (this.deltaY > 100 || (isQuickAction && this.deltaY > 15)) {
-                    this.prev();
-                } else {
-                    this.revert();
-                }
-
-                document.removeEventListener('touchmove', this.onTouchMove);
-                document.removeEventListener('touchend', this.onTouchEnd);
-                document.removeEventListener('mousemove', this.onTouchMove);
-                document.removeEventListener('mouseup', this.onTouchEnd);
-            },
             next() {
                 var page = this.currentPage;
                 if (this.currentPage < this.childrenCount) page++;
@@ -76,16 +43,50 @@
                 if (this.currentPage > 1) page--;
                 this.setPage(page);
             },
-            revert() {
-                this.setPage(this.currentPage);
-            },
             setPage(page) {
                 this.lastPage = this.currentPage;
                 this.currentPage = page;
                 this.translateY = -(this.currentPage - 1) * this.height;
                 this._onTransitionStart();
             },
-            getPageY(e) {
+            _onTouchStart(e) {
+                this.startY = this._getPageY(e);
+                this.height = this.$el.clientHeight;
+                this.startTranslateY = this.translateY;
+                this.startTime = new Date().getTime();
+                this.dragging = true;
+                document.addEventListener('touchmove', this._onTouchMove);
+                document.addEventListener('touchend', this._onTouchEnd);
+                document.addEventListener('mousemove', this._onTouchMove);
+                document.addEventListener('mouseup', this._onTouchEnd);
+            },
+            _onTouchMove(e) {
+                this.deltaY = this._getPageY(e) - this.startY;
+
+                if (!this.performanceMode) {
+                    this.translateY = this.startTranslateY + this.deltaY;
+                }
+            },
+            _onTouchEnd(e) {
+                this.dragging = false;
+                var isQuickAction = new Date().getTime() - this.startTime < 1000;
+                if (this.deltaY < -100 || (isQuickAction && this.deltaY < -15)) {
+                    this.next();
+                } else if (this.deltaY > 100 || (isQuickAction && this.deltaY > 15)) {
+                    this.prev();
+                } else {
+                    this._revert();
+                }
+
+                document.removeEventListener('touchmove', this._onTouchMove);
+                document.removeEventListener('touchend', this._onTouchEnd);
+                document.removeEventListener('mousemove', this._onTouchMove);
+                document.removeEventListener('mouseup', this._onTouchEnd);
+            },
+            _revert() {
+                this.setPage(this.currentPage);
+            },
+            _getPageY(e) {
                 return e.changedTouches ? e.changedTouches[0].pageY : e.pageY;
             },
             _onTransitionStart() {
