cd88bd38d395b65e1120f6c19f6357e0822eb7cd
Weilao <qqq123026689@126.com>
2016-06-05 22:35:22 +0800

    remove unnecessary html attribute

--- src/vue-swiper.vue
+++ src/vue-swiper.vue
@@ -1,4 +1,4 @@
-<template xmlns:v-el="http://www.w3.org/1999/xhtml">
+<template>
     <div class="swiper"
          :class="[direction, {'dragging': dragging}]"
          @touchstart="_onTouchStart"
